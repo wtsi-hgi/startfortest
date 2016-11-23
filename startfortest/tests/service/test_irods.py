bc23bdf814b951132618dec65670059357591d1f
Colin Nolan <colin.nolan@sanger.ac.uk>
2016-11-23 11:54:07 +0000

    Fixes Travis CI setup.

--- startfortest/tests/service/test_irods.py
+++ startfortest/tests/service/test_irods.py
@@ -1,11 +1,8 @@
-import os
+import shutil
 import unittest
 from abc import ABCMeta
 
-import shutil
-
 from startfortest.service.irods import Irods4_1_10Controller
-from startfortest.service.irods import IrodsDockerisedService
 from startfortest.tests.service._common import TestDockerisedServiceControllerSubclass, create_tests, ControllerType
 from testwithirods.helpers import SetupHelper
 from testwithirods.proxies import ICommandProxyController
