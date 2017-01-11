cd1b7cd7037161a14bb3749873adc019b6aa5103
Colin Nolan <colin.nolan@sanger.ac.uk>
2017-01-11 13:05:56 +0000

    Changes iRODS __init__.py module to explicitly list imports.

--- useintest/predefined/irods/__init__.py
+++ useintest/predefined/irods/__init__.py
@@ -1,5 +1,9 @@
-from useintest.predefined.irods.executables import *
-from useintest.predefined.irods.services import *
-from useintest.predefined.irods.models import *
-from useintest.predefined.irods.helpers import *
-from useintest.predefined.irods.setup import *
+from useintest.predefined.irods.executables import IrodsBaseExecutablesController, Irods3_3_1ExecutablesController,\
+    Irods4_1_8ExecutablesController, Irods4_1_9ExecutablesController, Irods4_1_10ExecutablesController, \
+    IrodsExecutablesController, irods_executables_controllers_and_versions, irods_executables_controllers
+from useintest.predefined.irods.helpers import AccessLevel, IrodsSetupHelper
+from useintest.predefined.irods.models import IrodsResource, IrodsUser, IrodsDockerisedService
+from useintest.predefined.irods.services import IrodsBaseServiceController, Irods3ServiceController, \
+    Irods4ServiceController, Irods3_3_1ServiceController, Irods4_1_8ServiceController, Irods4_1_9ServiceController, \
+    Irods4_1_10ServiceController, IrodsServiceController, irods_service_controllers
+from useintest.predefined.irods.setup import setup_irods
