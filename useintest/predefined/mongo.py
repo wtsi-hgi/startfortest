b3f783f07853abb4a0db511b7f04b7188c0bc00a
Colin Nolan <colin.nolan@sanger.ac.uk>
2017-05-09 15:00:25 +0100

    Detects full disk when starting up Mongo.

--- useintest/predefined/mongo.py
+++ useintest/predefined/mongo.py
@@ -3,7 +3,8 @@ from useintest.services._builders import DockerisedServiceControllerTypeBuilder
 common_setup = {
     "repository": "mongo",
     "start_detector": lambda log_line: "waiting for connections on port" in log_line,
-    "persistent_error_detector": lambda log_line: "error creating journal dir" in log_line,
+    "persistent_error_detector": lambda log_line: "error creating journal dir" in log_line
+                                                  or "No space left on device" in log_line,
     "ports": [27017]
 }
 
