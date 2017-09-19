8cfa32541af005a4ffae9740ceb1800e5982e060
Colin Nolan <colin.nolan@sanger.ac.uk>
2017-09-19 16:29:22 +0100

    Creates helper module for dealing with GitLab.

---
+++ useintest/predefined/gitlab/helpers.py
@@ -0,0 +1,89 @@
+from enum import Enum, unique
+from tempfile import NamedTemporaryFile
+from threading import Lock
+
+import os
+from typing import Callable, Optional, Dict
+
+from Crypto.PublicKey import RSA
+
+
+@unique
+class _KeyType(Enum):
+    """"
+    Types of keys.
+    """
+    PRIVATE = "private"
+    PUBLIC = "public"
+
+
+class SshKey:
+    """
+    Wrapper to help use an SSH key.
+    """
+    @property
+    def private_key(self) -> str:
+        return self._key.exportKey()
+
+    @property
+    def public_key(self) -> str:
+        return self._key.publickey()
+
+    @property
+    def private_key_file(self) -> str:
+        return self._lazy_get(_KeyType.PRIVATE, lambda: self.private_key)
+
+    @property
+    def public_key_file(self) -> str:
+        return self._lazy_get(_KeyType.PUBLIC, lambda: self.public_key)
+
+    def __init__(self, key_length: int=2048):
+        """
+        Constructor.
+        :param key_length: length of the RSA key to generate.
+        """
+        self._key = RSA.generate(key_length)
+        self._locks = {
+            _KeyType.PRIVATE: Lock(),
+            _KeyType.PUBLIC: Lock()
+        }
+        self._value_cache: Dict[Optional[str]] = {
+            _KeyType.PRIVATE: None,
+            _KeyType.PUBLIC: None
+        }
+
+    def __enter__(self):
+        pass
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.tear_down()
+
+    def tear_down(self):
+        """
+        Removes temp files managed by this instance.
+        """
+        for key_type, lock in self._locks.items():
+            with lock:
+                key_file = self._value_cache[key_type]
+                if key_file is not None:
+                    try:
+                        os.remove(key_file)
+                    except OSError:
+                        pass
+
+    def _lazy_get(self, key_type: _KeyType, data_source: Callable[[], str]) -> str:
+        """
+        Lazily gets the value associated to the given key type, generating it from the given data source and saving it
+        to the value cache if it has not been set.
+        :param key_type: the type of the key
+        :param data_source: the source to generate the value if not generated
+        :return: the value associated to the given key
+        """
+        if self._value_cache[key_type] is None:
+            with self._locks[key_type]:
+                if self._value_cache[key_type] is None:
+                    temp_file = NamedTemporaryFile()
+                    with open(temp_file, "w") as file:
+                        file.write(data_source())
+                    self._value_cache[key_type] = temp_file
+        return self._value_cache[key_type]
