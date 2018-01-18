d423b560f284b8d643db610d61efa31b6b4c62a0
Colin Nolan <colin.nolan@sanger.ac.uk>
2018-01-18 17:14:38 +0000

    Updates docs.

--- useintest/tests/predefined/consul/test_consul.py
+++ useintest/tests/predefined/consul/test_consul.py
@@ -22,18 +22,18 @@ class _TestConsulServiceController(
         service = self._start_service()     # type: ConsulDockerisedService
         self.assertIsInstance(service, ConsulDockerisedService)
         consul_client = service.create_consul_client()
-        self._test_consul_client(consul_client)
+        self._test_client(consul_client)
 
     def test_setup_environment(self):
         service = self._start_service()     # type: ConsulDockerisedService
         service.setup_environment()
         consul_client = Consul()
-        self._test_consul_client(consul_client)
+        self._test_client(consul_client)
 
-    def _test_consul_client(self, consul_client: Consul):
+    def _test_client(self, consul_client: Consul):
         """
-        TODO
-        :param consul_client:
+        Tests that the given Consul client connects to Consul
+        :param consul_client: the Consul client to test
         """
         consul_client.kv.put(_TEST_KEY, _TEST_VALUE)
         self.assertEqual(_TEST_VALUE, consul_client.kv.get(_TEST_KEY)[1]["Value"].decode("utf-8"))
